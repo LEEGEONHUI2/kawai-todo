897ee7072e97e745658d554a426a3eb0bdaf78ca
GEONHUI LEE <lee@GEONHUIui-MacBook-Pro.local>
2018-08-23 00:29:04 +0900

    Android elevation

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions, Platform } from 'react-native';
+
+const { hight, width } = Dimensions.get("window");
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar barStyle="light-content" />
+        <Text style={styles.title}>Kawai Todo</Text>
+        <View style={styles.card}>
+          {/*   placeholderTextColor={"#000000"}  */}
+          <TextInput style={styles.input} placeholder={"New Todo"} underlineColorAndroid='transparent' />
+        </View>
       </View>
     );
   }
@@ -14,8 +21,39 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F23657',
     alignItems: 'center',
-    justifyContent: 'center',
+    //justifyContent: 'center', //세로 가운대
+  },
+  title: {
+    color: "white",
+    fontSize: 30,
+    marginTop: 50,
+    fontWeight: "200",
+    marginBottom: 30,
+  },
+  card: {
+    backgroundColor: "white",
+    flex: 1,
+    width: width - 25,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    ...Platform.select({
+      ios: {
+        shadowColor: "rgb(50,50,50)",
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        shadowOffset: {
+          hight: -1,
+          width: 0
+        }
+      },
+      android: {
+        elevation: 3
+      }
+    })
+  },
+  input: {
+
   },
 });
